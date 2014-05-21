371197fea4397f616c7e0db58d74d0701c30201c
Michael Bayne <mdb@samskivert.com>
2014-05-21 15:56:17 -0700

    Add count of source files by type to describeSelf.

--- src/main/scala/scaled/project/MavenProject.scala
+++ src/main/scala/scaled/project/MavenProject.scala
@@ -4,14 +4,18 @@
 
 package scaled.project
 
-import java.nio.file.Path
+import com.google.common.collect.{Multiset, HashMultiset}
+import java.nio.file.attribute.BasicFileAttributes
+import java.nio.file.{Files, FileVisitResult, Path, SimpleFileVisitor}
 import pomutil.{DependResolver, Dependency, POM}
 import reactual.Future
+import scala.collection.mutable.{Map => MMap}
 import scaled._
 import scaled.util.BufferBuilder
 
 class MavenProject (root :Path, metaSvc :MetaService, projectSvc :ProjectService)
     extends FileProject(root, metaSvc) {
+  import scala.collection.convert.WrapAsScala._
 
   private[this] val pomFile = root.resolve("pom.xml")
   // TODO: reload the POM if it changes? restart the compiler if so...
@@ -23,8 +27,29 @@ class MavenProject (root :Path, metaSvc :MetaService, projectSvc :ProjectService
   override def id = Some(pom.id)
   override def sourceURL = pom.scm.connection.map(stripSCM)
 
-  // TODO: figure out what kind of compiler we should use based on what?
-  //       plugins in POM, source files, chicken sacrifice?
+  override def describeSelf (bb :BufferBuilder) {
+    super.describeSelf(bb)
+
+    bb.addSubHeader("Maven Info")
+    bb.addSection("Source dirs:")
+    bb.addKeysValues("compile: " -> sourceDirs.mkString(" "),
+                     "test: "    -> testSourceDirs.mkString(" "))
+    val srcsum = summarizeSources
+    if (!srcsum.isEmpty) {
+      bb.addSection("Source files:")
+      bb.addKeysValues(srcsum.entrySet.map(
+        e => (s"${e.getElement}: ", e.getCount.toString)).toSeq :_*)
+    }
+    bb.addSection("Output dirs:")
+    bb.addKeysValues("compile: " -> outputDir.toString,
+                     "test: "    -> testOutputDir.toString)
+    bb.addSection("Compile classpath:")
+    buildClasspath foreach { p => bb.add(p.toString) }
+    bb.addSection("Test classpath:")
+    testClasspath foreach { p => bb.add(p.toString) }
+  }
+
+  // TODO: use summarizeSources to determine whether to use a Java or Scala compiler
   override protected def createCompiler () = new ScalaCompiler(this) {
     def sourceDirs = MavenProject.this.sourceDirs
     def buildClasspath = MavenProject.this.buildClasspath
@@ -37,20 +62,22 @@ class MavenProject (root :Path, metaSvc :MetaService, projectSvc :ProjectService
 
   override protected def ignores = MavenProject.mavenIgnores
 
-  override def describeSelf (bb :BufferBuilder) {
-    super.describeSelf(bb)
-
-    bb.addSubHeader("Maven Info")
-    bb.addSection("Source dirs:")
-    bb.addKeysValues("compile: " -> sourceDirs.mkString(" "),
-                     "test: "    -> testSourceDirs.mkString(" "))
-    bb.addSection("Output dirs:")
-    bb.addKeysValues("compile: " -> outputDir.toString,
-                     "test: "    -> testOutputDir.toString)
-    bb.addSection("Compile classpath:")
-    buildClasspath foreach { p => bb.add(p.toString) }
-    bb.addSection("Test classpath:")
-    testClasspath foreach { p => bb.add(p.toString) }
+  def summarizeSources :Multiset[String] = {
+    val counts = HashMultiset.create[String](2)
+    sourceDirs foreach { dir =>
+      // TODO: should we be following symlinks? likely so...
+      Files.walkFileTree(dir, new SimpleFileVisitor[Path]() {
+        override def visitFile (file :Path, attrs :BasicFileAttributes) = {
+          if (!attrs.isDirectory) {
+            val fname = file.getFileName.toString
+            if (fname endsWith ".java") counts.add(".java")
+            else if (fname endsWith ".scala") counts.add(".scala")
+          }
+          FileVisitResult.CONTINUE
+        }
+      })
+    }
+    counts
   }
 
   def sourceDirs :Seq[Path] = Seq(buildDir("sourceDirectory", "src/main"))
