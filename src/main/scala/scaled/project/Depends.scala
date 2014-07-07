c3504a1fe7762891391b7e98edfbb5927328acd0
Michael Bayne <mdb@samskivert.com>
2014-07-07 15:35:01 -0700

    Properly handle classified depends.
    The maze of passages, it twists.

--- src/main/scala/scaled/project/Depends.scala
+++ src/main/scala/scaled/project/Depends.scala
@@ -17,7 +17,7 @@ abstract class Depends (projectSvc :ProjectService) {
 
   /** Resolves depends for [[pom]], using `projectSvc` to resolve projects known to Scaled in lieu
     * of artifacts in the local Maven repo. */
-  def transitive :Seq[Project.Id] = transitiveDepends(DependResolver.Compile) map(toId)
+  def transitive :Seq[Project.Id] = transitiveDepends(DependResolver.Compile) flatMap(toId)
 
   def buildClasspath = classpath(DependResolver.Compile)
   def testClasspath = classpath(DependResolver.Test)
@@ -26,25 +26,33 @@ abstract class Depends (projectSvc :ProjectService) {
   // TODO: infer the desired Java version from the maven-compiler-plugin POM section?
   def platformDepend = PlatformId(JavaPlatform, JDK.thisJDK.majorVersion)
 
-  private def toId (dep :Dependency) = RepoId(MavenRepo, dep.groupId, dep.artifactId, dep.version)
+  // if a dependency has a classifier, don't turn it into a Scaled project
+  private def toId (dep :Dependency) = dep.classifier match {
+    case None => Some(mkId(dep))
+    case _    => None
+  }
+  private def mkId (dep :Dependency) = RepoId(MavenRepo, dep.groupId, dep.artifactId, dep.version)
 
   protected def classpath (scope :DependResolver.Scope) :Seq[Path] =
-    for (dep <- transitiveDepends(scope) ; id = toId(dep))
-    yield projectSvc.projectFor(id) match {
-      case Some(proj :JavaProject) => proj.classes
-      case _                       => dep.systemPath match {
-        case Some(path) => Paths.get(path)
-        case _          => Maven.resolveClasses(id)
+    transitiveDepends(scope) map { dep => toId(dep) match {
+      case Some(id) => projectSvc.projectFor(id) match {
+        case Some(proj :JavaProject) => proj.classes
+        case _                       => dep.systemPath match {
+          case Some(path) => Paths.get(path)
+          case _          => Maven.resolveClasses(id)
+        }
       }
-    }
+      case None => Maven.resolve(mkId(dep), "jar", dep.classifier)
+    }}
 
   private def transitiveDepends (scope :DependResolver.Scope) = new DependResolver(pom) {
     // check whether this dependency is known to Scaled, and use the known version's POM instead of
     // the default POM (which comes from ~/.m2 and may be stale for snapshot depends)
-    override def localDep (dep :Dependency) = projectPOM(toId(dep)) orElse super.localDep(dep)
+    override def localDep (dep :Dependency) =
+      toId(dep).flatMap(projectPOM) orElse super.localDep(dep)
     private def projectPOM (id :RepoId) = projectSvc.projectFor(id) match {
       case Some(proj :MavenProject) => Some(proj.pom)
-      case _ => None
+      case _                        => None
     }
   }.resolve(scope)
 }
