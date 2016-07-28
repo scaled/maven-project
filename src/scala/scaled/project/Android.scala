2dad38c16960eb79b086299c94ff61e81da19c67
Michael Bayne <mdb@fb.com>
2016-07-27 18:32:54 -0700

    Robustness++.
    Previously we could end up creating the extracted dir but not unpacking
    the jars for whatever reason and then we'd never try again because the
    directory was already there. Now we only assume goodness if the
    directory has at least one jar in it (still not perfect, but better).

--- src/scala/scaled/project/Android.scala
+++ src/scala/scaled/project/Android.scala
@@ -16,35 +16,44 @@ object Android {
 
   def jarsForAar (dep :Dependency) :SeqV[Path] = {
     val aarPath = Maven.resolve(dep)
-    val m2dir = aarPath.getParent()
-    val xdir = m2dir.resolve("extracted")
-    // if we haven't extracted the jar files from this aar yet, do so
-    if (!Files.exists(xdir)) {
-      Files.createDirectory(xdir);
-      val aar = new JarFile(aarPath.toFile)
-      val enumEntries = aar.entries()
-      while (enumEntries.hasMoreElements) {
-        val entry = enumEntries.nextElement.asInstanceOf[JarEntry]
-        val entryName = entry.getName
-        if (entryName.endsWith(".jar")) {
-          // TODO: this is probably not a good idea, but I'm going to do it anyway in the name of
-          // expedience...
-          val entryFileName = entryName.lastIndexOf('/') match {
-            case -1 => entryName
-            case n  => entryName.substring(n+1)
-          }
-          val jarPath = xdir.resolve(entryFileName)
-          val is = aar.getInputStream(entry)
-          val fos = new FileOutputStream(jarPath.toFile)
-          while (is.available() > 0) {  // write contents of 'is' to 'fos'
-            fos.write(is.read())
-          }
-          fos.close()
-          is.close()
+    val xdir = aarPath.getParent().resolve("extracted")
+    def listJars = Seq.view(Files.list(xdir).collect(Collectors.toList[Path]))
+
+    // if the 'extracted' directory exists and contains jars, then we're done
+    val xdirExists = Files.exists(xdir)
+    if (xdirExists) {
+      val jars = listJars
+      if (!jars.isEmpty) return jars
+    }
+
+    // we may or may not need to create the 'extracted' directory
+    if (!xdirExists) Files.createDirectory(xdir);
+
+    // now we can extract the jar files from this aar; such android, so design
+    val aar = new JarFile(aarPath.toFile)
+    val enumEntries = aar.entries()
+    while (enumEntries.hasMoreElements) {
+      val entry = enumEntries.nextElement.asInstanceOf[JarEntry]
+      val entryName = entry.getName
+      if (entryName.endsWith(".jar")) {
+        // TODO: this is probably not a good idea, but I'm going to do it anyway in the name of
+        // expedience...
+        val entryFileName = entryName.lastIndexOf('/') match {
+          case -1 => entryName
+          case n  => entryName.substring(n+1)
         }
+        val jarPath = xdir.resolve(entryFileName)
+        val is = aar.getInputStream(entry)
+        val fos = new FileOutputStream(jarPath.toFile)
+        while (is.available() > 0) {  // write contents of 'is' to 'fos'
+          fos.write(is.read())
+        }
+        fos.close()
+        is.close()
       }
     }
-    // now list all the jars in xdir
-    Seq.view(Files.list(xdir).collect(Collectors.toList[Path]))
+
+    // finally enumerate the jars we just unpacked
+    listJars
   }
 }
